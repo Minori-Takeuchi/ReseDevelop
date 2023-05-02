a4a37f87cb64060f8dcb2c047ef6d8c3dfd2f721
EC2 Default User <ec2-user@ip-172-31-5-249.ap-northeast-1.compute.internal>
2023-05-02 01:25:28 +0000

    test10

--- backend/config/database.php
+++ backend/config/database.php
@@ -82,6 +82,20 @@ return [
             ]) : [],
         ],
 
+        'circleci' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
